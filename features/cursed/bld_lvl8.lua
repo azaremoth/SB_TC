eef83d14499abf566a63dbf9cf771dae130bb0fc
azaremoth <azaremoth@users.noreply.github.com>
2019-08-30 23:34:20 +0200

    adding more features for modular buildings

--- features/cursed/bld_lvl8.lua
+++ features/cursed/bld_lvl8.lua
@@ -1,28 +1,18 @@
 local objects = {
-	"features/modtower/bld_L0_F1.s3o",
-	"features/modtower/bld_L0_F2.s3o",
-	"features/modtower/bld_L1_F1.s3o",
-	"features/modtower/bld_L1_F2.s3o",
-	"features/modtower/bld_L2_F1.s3o",
-	"features/modtower/bld_L2_F2.s3o",
-	"features/modtower/bld_L3_F1.s3o",
-	"features/modtower/bld_L3_F2.s3o",
-	"features/modtower/bld_L4_F1.s3o",
-	"features/modtower/bld_L4_F2.s3o",
-	"features/modtower/bld_L5_F1.s3o",
-	"features/modtower/bld_L5_F2.s3o",
-	"features/modtower/bld_L6_F1.s3o",
-	"features/modtower/bld_L6_F2.s3o",
-	"features/modtower/bld_L7_F1.s3o",
-	"features/modtower/bld_L7_F2.s3o",
-	"features/modtower/bld_L8_F1.s3o",
-	"features/modtower/bld_L8_F2.s3o",
+	"features/modtower/bld_L8_1.s3o",
+	"features/modtower/bld_L8_2.s3o",
+	"features/modtower/bld_L8_3.s3o",
+	"features/modtower/bld_L8_4.s3o",
+	"features/modtower/bld_L8_5.s3o",
+	"features/modtower/bld_L8_6.s3o",
+	"features/modtower/bld_L8_7.s3o",
+	"features/modtower/bld_L8_8.s3o",		
 }
 
 local BuildingModuleDef = {}
 local function createBuildingModuleDef(i)
-  BuildingModuleDef["buildingfeature_" .. i] = {
-     description = [[Building Attachment]],
+  BuildingModuleDef["buildingmodule_lvl8_" .. i] = {
+     description = [[Building]],
 	 autoreclaimable = false,
      blocking    = true,
      burnable    = false,
@@ -35,10 +25,10 @@ local function createBuildingModuleDef(i)
      reclaimTime = 9999999999,
      mass        = 9999999999,
      object = objects[(i % #objects) + 1] ,
-     footprintX  = 1,
-     footprintZ  = 1,
---	 collisionVolumeType 	= "Box",
---	 collisionVolumeScales 	= "3 3 3",
+     footprintX  = 6,
+     footprintZ  = 6,
+	 collisionVolumeType 	= "Box",
+	 collisionVolumeScales 	= "60 30 60",
 --	 collisionVolumeOffsets 	= "0 0 0", 
 	 customparams = { 
 			normaltex 		= "unittextures/normalmaps/atlas_euf_buildings_normal.png",
@@ -47,7 +37,7 @@ local function createBuildingModuleDef(i)
   }
 end
 
-for i=1,18 do
+for i=1,8 do
   createBuildingModuleDef(i)
 end
 
